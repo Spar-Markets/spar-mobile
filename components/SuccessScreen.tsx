8787bb84ff6d5171fc44531b3b4529dd99f1bfce
jacksonbelanger <jacksonlbelanger@gmail.com>
2024-01-22 22:30:14 -0500

    first Plaid commit

---
+++ components/SuccessScreen.tsx
@@ -0,0 +1,39 @@
+import React, {useState, useEffect, useCallback} from 'react';
+import {
+  Text,
+  View,
+  ToastAndroid,
+  Platform,
+  Alert,
+} from 'react-native';
+var styles = require('./style');
+
+const SuccessScreen = ({ navigation, route }: any) => {
+  const [data, setData] = useState(null);
+
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={styles.heading}>
+        <Text style={styles.titleText}>Balance Response</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.baseText}>
+            {
+              JSON.stringify(data)
+            }
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+function notifyMessage(msg: string) {
+  if (Platform.OS === 'android') {
+    ToastAndroid.show(msg, ToastAndroid.SHORT);
+  } else {
+    Alert.alert(msg);
+  }
+}
+
+export default SuccessScreen;
---
+++ constants/global.js
@@ -0,0 +1,8 @@
+const ip = "3.95.164.69";
+const port = 3000;
+const mongoPort = 8000;
+
+export const ip1 = ip
+export const serverUrl = `http://${ip}:${port}`;
+export const mongoServerUrl = `http://${ip}:${mongoPort}`;
+export const plaidClientID = "65833a47f1ae5c001b9d8fee";
