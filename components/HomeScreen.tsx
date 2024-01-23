63a84160de92fc817a147e491f5401c50af99198
grantdrink <grantjdrinkwater@gmail.com>
2024-01-22 23:00:49 -0500

    server with plaid

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -3,24 +3,27 @@ import React, {useState, useEffect, useCallback} from 'react';
 import { Platform, View, Text, StyleSheet } from 'react-native';
 import {PlaidLink, LinkExit, LinkSuccess } from 'react-native-plaid-link-sdk';
 import { serverUrl } from '../constants/global';
+import axios from 'axios';
 
 var styles = require('./style');
-
 
 const HomeScreen = ({ navigation }: any) => {
   const [linkToken, setLinkToken] = useState("");
   const address = Platform.OS === 'ios' ? 'localhost' : '10.0.2.2';
 
   const createLinkToken = useCallback(async () => {
-    await fetch(`${serverUrl}/create_link_token`, {
+    console.log("linkinitiative")
+    await fetch(serverUrl+"/createLinkToken", {
     method: "POST",
     headers: {
+      "Environment":"sandbox",
       "Content-Type": "application/json"
     },
     body: JSON.stringify({ address: address })
     })
     .then((response) => response.json())
     .then((data) => {
+      console.log("log empty " + data.link_token)
       setLinkToken(data.link_token);
     })
     .catch((err) => {
@@ -29,7 +32,8 @@ const HomeScreen = ({ navigation }: any) => {
   }, [setLinkToken])
 
   useEffect(() => {
-    if (linkToken == null) {
+    if (linkToken == "") {
+      console.log("GettingLinkToken")
       createLinkToken();
     }
   }, [linkToken]);
@@ -37,16 +41,18 @@ const HomeScreen = ({ navigation }: any) => {
   return (
     <View style={{flex: 1}}>
       <View style={styles.heading}>
-        <Text style={styles.titleText}>Tiny Quickstart – React Native</Text>
+        <Text style={styles.titleText}>Spar</Text>
       </View>
       <View style={styles.bottom}>
         <PlaidLink
           tokenConfig={{
-            token: linkToken,
+            token: `sandbox-${linkToken}`, // Add the environment name (sandbox) here
             noLoadingState: false,
           }}
+          
           onSuccess={async (success: LinkSuccess) => {
-            await fetch(`http://${address}:8080/api/exchange_public_token`, {
+            console.log(linkToken)
+            await fetch(`${serverUrl}/exchangePublicToken`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -54,11 +60,15 @@ const HomeScreen = ({ navigation }: any) => {
             body: JSON.stringify({ public_token: success.publicToken }),
             })
             .catch((err) => {
+              console.log(linkToken)
+
               console.log(err);
             });
             navigation.navigate('Success', success);
           }}
           onExit={(response: LinkExit) => {
+            console.log(linkToken)
+
             console.log(response);
           }}>
           <View style={styles.buttonContainer}>
