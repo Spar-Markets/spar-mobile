5ed21b80846c5c153cea387ac16af62432b3b785
grantdrink <grantjdrinkwater@gmail.com>
2024-03-18 00:07:48 -0400

    fly server integrated

--- constants/global.js
+++ constants/global.js
@@ -2,7 +2,6 @@ const ip = "https://spar-server.fly.dev";
 const port = 3000;
 // const mongoPort = 8000;
 
-export const ip1 = ip
-export const serverUrl = `${ip}:${port}`;
+export const serverUrl = `${ip}`;
 // export const mongoServerUrl = `http://${ip}:${mongoPort}`;
 export const plaidClientID = "65833a47f1ae5c001b9d8fee";
