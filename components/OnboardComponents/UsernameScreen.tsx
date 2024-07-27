ad12f9c3b577013595c7a5622897e205effdeeac
grantdrink <grantjdrinkwater@gmail.com>
2024-07-26 21:07:16 -0400

    iamge

--- components/OnboardComponents/UsernameScreen.tsx
+++ components/OnboardComponents/UsernameScreen.tsx
@@ -22,7 +22,11 @@ import useUserDetails from '../../hooks/useUserDetails';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {useDispatch} from 'react-redux';
-import {setUserID, setUserIsMade, setUsername} from '../../GlobalDataManagment/userSlice';
+import {
+  setUserID,
+  setUserIsMade,
+  setUsername,
+} from '../../GlobalDataManagment/userSlice';
 
 const UsernameScreen = (props: any) => {
   // Layout and Style Initialization
@@ -65,16 +69,15 @@ const UsernameScreen = (props: any) => {
       );
 
       if (credentials.user) {
-
-
         // Array of profile images
-        const profileImages = ["profile1", "profile2", "profile3"];
-    
+        const profileImages = ['profile1', 'profile2', 'profile3'];
+
         // Select a random profile image
-        const randomImage = profileImages[Math.floor(Math.random() * profileImages.length)];
+        const randomImage =
+          profileImages[Math.floor(Math.random() * profileImages.length)];
 
         // Set the profile image string
-        await AsyncStorage.setItem('profileImage', `../../assets/images/${randomImage}`);
+        await AsyncStorage.setItem('defaultProfileImage', randomImage);
 
         console.log('User profile image initialized in AsyncStorage');
 
@@ -83,7 +86,7 @@ const UsernameScreen = (props: any) => {
           userID: (credentials.user as any).uid,
           username: usernameInput,
           defaultProfileImage: randomImage,
-          hasDefaultProfileImage: "true"
+          hasDefaultProfileImage: 'true',
         });
 
         // Sets userID globally in async
@@ -97,15 +100,16 @@ const UsernameScreen = (props: any) => {
           ).then(() => {
             dispatch(setUserIsMade(true));
             dispatch(setUserID((credentials.user as any).uid));
-            dispatch(setUsername(usernameInput))
+            dispatch(setUsername(usernameInput));
           });
           try {
             await AsyncStorage.setItem('defaultProfileImage', randomImage);
             await AsyncStorage.setItem('hasDefaultProfileImage', 'true');
-
-            
           } catch (error) {
-            console.error('Error initializing user profile image in AsyncStorage', error);
+            console.error(
+              'Error initializing user profile image in AsyncStorage',
+              error,
+            );
           }
         }
       }
